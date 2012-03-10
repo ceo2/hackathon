7327af3d5161707bae0ea694d6d8d55f25eee33c
marc <marc@192.168.0.27>
2012-03-11 00:01:55 +0100

    erster Versuch

--- MP3AVS/src/MP3AVS.scala
+++ MP3AVS/src/MP3AVS.scala
@@ -1,5 +1,29 @@
 object MP3AVS {
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = 
+  {
+    println ("Bitte geben Sie das Verzeichnis ein:")
+    val directoryPath = readLine()
+    
+    var musicCollection = scanMusicCollection(directoryPath)
+    
+    println ("Die Musiksammlung hat folgende Interpreter: ")
+    for (interpret <- musicCollection)
+    	println(interpret)
+  //  giveExistingAlbums()
+  //  diffOfAlums()
+   // Output()
+    
+    
+    
+
+  }
+  
+  def scanMusicCollection(path: String) : Array[String] =
+  {
+    var musicCol = Array(path, "zweiter Eintrag")
+    musicCol
+  }
+ 
 
 }
